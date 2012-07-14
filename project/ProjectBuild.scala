96bdd7a9316b5b6cb4dc4111229ffd6b72d3ed79
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-14 08:04:43 +0100

    Initial commit of empty project config.

---
+++ project/ProjectBuild.scala
@@ -0,0 +1,40 @@
+import sbt._
+import Keys._
+
+import com.typesafe.sbteclipse.core.EclipsePlugin._
+
+object ProjectBuild extends Build {
+  import BuildSettings._
+  import ShellPrompt._
+  import Dependencies._
+
+  lazy val root = Project("root", 
+    file("."),
+    settings = buildSettings ++ Unidoc.settings ++ Seq (
+      scaladocOptions := Seq(),
+      // Disable publish and publish-local for empty root project
+      publish := {},
+      publishLocal := {}
+    )
+  ) aggregate(coreProject, liftProject)
+
+  lazy val coreProject: Project = Project("core",
+    file("scala-utilities-core"),
+      delegates = root :: Nil,
+      settings = buildSettings ++ Seq(libraryDependencies ++= coreProjectDeps,
+      name := "scala-utilities-core"
+    )
+  )
+
+  lazy val liftProject: Project = Project("lift", 
+    file("scala-utilities-lift"),
+    settings = buildSettings ++ Seq(libraryDependencies ++= liftProjectDeps,
+      name := "scala-utilities-lift"
+    )
+  ) dependsOn(coreProject)
+
+  val commonDeps = testDeps ++ jodaDeps
+  val coreProjectDeps = commonDeps
+  val liftProjectDeps = commonDeps ++ liftDeps ++ jettyDeps
+}
+
---
+++ project/plugins.sbt
@@ -0,0 +1,3 @@
+// Eclipse SBT
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.0-RC1")
+
